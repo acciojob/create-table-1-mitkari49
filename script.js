4a1a7b68e6c55d56898d1f6202db2fc7b1f7ac3e
mitkari49 <93242478+mitkari49@users.noreply.github.com>
2024-11-22 13:07:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let newRow = table.insertRow(0); // insert new row at top
 let cell1 = newRow.insertCell(0);
 let cell2 = newRow.insertCell(1);
 	
-	 cell1.innerHTML = "New Row cell1";
-	 cell2.innerHTML = "New Row cell2";
+	 cell1.innerHTML = "New Cell1";
+	 cell2.innerHTML = "New Cell2";
 	
 }
